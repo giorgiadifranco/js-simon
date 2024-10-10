c832daa3f8b7b4921e8c78c861b1d4de1ef6e32a
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-10 15:56:35 +0200

    add function random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 console.log('it works');
+
+//const computerNumber = Math.floor(Math.random()*6);
+
+//creo una funzione che contenga una variabile per inserire un array di numeri
+//i numeri verranno generati da un ciclo 
+function RandomInt(size, maxNumber) {
+    let computerNumbers = [];
+
+    for (i = 0 ; i < 5; i++){
+        
+        computerNumbers.push(Math.floor(Math.random()*100));
+        
+    }
+    return computerNumbers;
+  }
+  let randomArray = RandomInt(size, maxNumber);
+console.log(randomArray);
+
+
+  
+  
